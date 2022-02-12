56d8d6b58bb0f3d9758aa6ea68a2b691959cb5f6
antipachita <nice.strogonov@inbox.ru>
2022-02-13 02:30:56 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -35,11 +35,37 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let array = [];
+    for (let i = 0; i< expr.length; i+=10){
+      array.push(expr.slice(i, i+10))
+    }
+    let filarr = []
+    for (let i = 0; i < array.length; i++) {
+       for (let n = 0; n < array[i].length; n++){
+         if (array[i][n] === '*') {
+           filarr.push(array[i].slice(n))
+           break
+         }
+         else if (array[i][n] === '1') {
+           filarr.push(array[i].slice(n))
+           break
+         }
+       }
+    }
+    let lastFiltеr = filarr.map(element => element.replace(/10/g, '.').replace(/11/g, '-'))
+    for (let i = 0; i < lastFiltеr.length; i++) {
+      for (let value in MORSE_TABLE) {
+        if (lastFiltеr[i] === value) {
+          lastFiltеr[i] = MORSE_TABLE[value];
+        }
+      }
+    }
+      return lastFiltеr.join('');
+    }
 
 module.exports = {
     decode
